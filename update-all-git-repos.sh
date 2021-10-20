4916b25e215d81686479e1c54d96b5cb6e0c149f
Ricardo Mendes <ricardolsmendes@gmail.com>
2021-10-19 21:14:07 -0300

    Update shebang and failure-related options

--- update-all-git-repos.sh
+++ update-all-git-repos.sh
@@ -1,17 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-for d in $(find . -mindepth 1 -maxdepth 1 -type d)
+set -eo pipefail
+
+
+subfolders=$(find . -mindepth 1 -maxdepth 1 -type d)
+
+for folder in $subfolders
 do
-  cd $d
+  cd $folder
   echo -e "\n========================================"
-  echo Processing $d
-  
+  echo "Processing $folder"
+
   echo -e "\n1. Updating current branch..."
   git pull
 
   echo -e "\n2. Updating objects and refs from origin..."
   git fetch --prune
-  
+
   echo -e "\n3. All branches:"
   git branch --all
 
