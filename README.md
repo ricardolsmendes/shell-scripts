efd43331b0cb4c430f5e075db786271c212340b3
Ricardo Mendes <ricardolsmendes@gmail.com>
2021-10-19 22:02:16 -0300

    Add Maven-related scripts

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ regular developer's tasks.
 
 Update all Git local repositories nested to a given directory by running
 `git pull` and `git fetch --prune` for each of them.
+
+## maven-prepare-release.sh
+
+Set up the version of a [Maven](https://maven.apache.org/)-based project by
+updating the parent and child `pom.xml` files, commit the changes to Git and
+create a tag as well.
+
+## maven-set-up-version.sh
+
+Set up the version of a [Maven](https://maven.apache.org/)-based project by
+updating the parent and child `pom.xml` files, and commit the changes to Git.
---
+++ maven-set-up-version.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# Input args:
+# - $1: the version ID, e.g.: 1.0.0 (mandatory)
+
+
+# Input validation
+if [[ $1 == "" ]]
+then
+  echo "The version ID (e.g.: 1.0.0) is mandatory"
+  exit 1
+fi
+
+
+# Variables
+versionId=$1
+version="${versionId}-SNAPSHOT"
+
+
+cd ..
+
+
+# Maven setup
+echo "[Maven] Setting $version as the new version..."
+mvn versions:set -DnewVersion="$version"
+
+echo -e '\n[Maven] Confirming the changes...'
+mvn versions:commit
+
+
+# Git commands
+echo -e '\n[Git] Staging the changes...'
+git add pom.xml **/pom.xml
+
+echo -e '\n[Git] Committing the changes...'
+git commit -m "chore: start working on version $versionId"
